2ba82fa83e5406f6c265f0c0feb8f3413308000b
almrashid <alrashidm@outlook.com>
2018-09-10 15:45:16 -0400

    pyramit

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -13,7 +13,26 @@ public class MakePyramid {
                          * * * * * *
 
         */
+        MakePyramid(9);
 
 
         }
+
+
+    public static void MakePyramid(int n){
+        int k = 2*n-2;
+        for (int i = 0; i < n; i++){
+
+            for (int j = 0; j<k;j++){
+                System.out.print(" ");
+            }
+
+            k = k-1;
+
+            for (int a = 0;a < i; a++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
   }
