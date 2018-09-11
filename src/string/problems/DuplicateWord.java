5cb096d4ed8078be8259dff69e7936662d461539
almrashid <alrashidm@outlook.com>
2018-09-11 04:20:28 -0400

    duplicate word

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -12,7 +15,24 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
+        DuplicateWords(st);
     }
 
+
+        public static  void DuplicateWords(String str){
+            Map<String,Integer> map = new HashMap<String,Integer>();
+            String[] arr = str.split(" ");
+
+            for (String x: arr) {
+                if (map.get(x) != null){
+                    map.put(x,map.get(x)+1);
+                }else {
+                    map.put(x,1);
+                }
+            }
+
+            for (Map.Entry entry: map.entrySet()) {
+                System.out.println(entry.getKey() +": "+entry.getValue());
+            }
+        }
 }
