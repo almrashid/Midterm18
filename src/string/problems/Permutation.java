fce4e42263293a0d9cfdb15816cb45d7047d217f
almrashid <alrashidm@outlook.com>
2018-09-11 04:25:51 -0400

    permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,5 +12,24 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+
+        listPermutations("","CAT");
     }
-}
+
+
+
+
+        public static void listPermutations(String pre,String word){
+            int length = word.length();
+            if (length == 0){
+                System.out.println(pre);
+            }else {
+                for (int i = 0; i<length;i++ ){
+                    listPermutations(pre + word.charAt(i),word.substring(0,i) + word.substring(i+1,length));
+                }
+
+            }
+
+        }
+
+    }
